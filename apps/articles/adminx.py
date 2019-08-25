9cd3e850e9af39783e4f7e7dfa57438443aff59c
wron <whisnos@163.com>
2019-08-25 18:01:53 +0800

    xadmin 添加搜索 过滤

--- apps/articles/adminx.py
+++ apps/articles/adminx.py
@@ -3,16 +3,17 @@ from articles.models import TagInfo, ArticleInfo
 
 
 class TagInfoXadmin(object):
-	list_display = ['name', 'add_time']
+	list_display = ['name', 'category','add_time']
+	list_filter = ['category']
 
-
-class ArtTagXadmin(object):
-	list_display = ['articleinfo', 'taginfo', 'add_time']
+# class ArtTagXadmin(object):
+# 	list_display = ['articleinfo', 'taginfo', 'add_time']
 
 
 class ArticleInfoXadmin(object):
 	list_display = ['title', 'click_num', 'cont_num', 'author', 'category', 'add_time']
 	style_fields = {'content': 'ueditor'}
+	search_fields = ['title']
 	# 添加附加选项表
 	# class ArtTagInlines(object):
 	# 	model = ArtTag
